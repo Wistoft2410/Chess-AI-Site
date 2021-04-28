cd4dff08034af1a5487ef9309da23a273dac115b
AndysonDK <28491857+AndysonDK@users.noreply.github.com>
2021-04-28 16:15:41 +0200

    better scripts

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source ./env/bin/activate
+. ./env/bin/activate
 flask run
 deactivate
 echo
--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 python3 -m venv env
 echo "export FLASK_ENV=development" >> ./env/bin/activate
 echo "export FLASK_APP=main.py" >> ./env/bin/activate
-source ./env/bin/activate
+. ./env/bin/activate
 pip install -r requirements.txt
 deactivate
