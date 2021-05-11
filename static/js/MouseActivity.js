96fe6af117bd3e563636276bd726c8b514dda5e9
Adam Wistoft <44964494+Wistoft2410@users.noreply.github.com>
2021-05-11 19:18:03 +0200

    Opdatering af game.html & MouseActivity

---
+++ static/js/MouseActivity.js
@@ -0,0 +1,10 @@
+var mX, mY;
+var path  = new Array();
+var index = 0;
+var Num = 0;
+var save  = false;
+var track = false;
+
+function setup() {
+
+  }
---
+++ static/js/sketch.js
@@ -0,0 +1,87 @@
+/*
+ * This is a good video, although I don't find him funny: https://www.youtube.com/watch?v=DZfv0YgLJ2Q
+ * Here is the code used in the video: https://github.com/Code-Bullet/Chess-AI
+  * */
+
+let movingPiece;
+let whitesMove;
+let board;
+
+let endGame
+
+let blackOrWhite;
+let difficulty;
+
+
+function setup() {
+  createCanvas(800, 800);
+  movingPiece = null;
+  whitesMove = true;
+  endGame = false;
+
+  // Retrieve cookie values
+  try {
+    blackOrWhite = JSON.parse(Cookies.get('black_or_white'));
+    difficulty = JSON.parse(Cookies.get('difficulty'));
+  } catch (err) {
+    // If an error occurs then we just use default settings    
+    blackOrWhite = true;
+    difficulty = 0;
+  }
+
+  board = new Board();
+}
+
+function draw() {
+  background(100);
+  board.display();
+
+  if (endGame) {
+    // Do something here when the game ends. Right now the only way it can end is where
+    // one player wins and the other loses, there is no draw like stalemate currently
+    if (whitesMove) {
+      document.getElementById("game-status-message").display=false;
+      console.log(`white player has been checkmated!!!`);
+      console.log("Which means that black player won!");
+    } else {
+
+      console.log(`black player has been checkmated!!!`);
+      console.log("Which means that white player won!");
+    }
+  }
+}
+
+function mousePressed() {
+  // There is apparently some bug with right click.
+  // It doens't make sence to respond to right click either,
+  // So we are only responding to left click!
+  if (!endGame && mouseButton === LEFT) {
+    const x = floor(mouseX / Board.TILE_SIZE);
+    const y = floor(mouseY / Board.TILE_SIZE);
+
+    movingPiece = board.getPiece(x, y);
+    // If the piece exists and the piece belongs to/is controlled by the current player,
+    // then the player is allowed to move that piece
+    if (movingPiece && movingPiece.white === whitesMove) movingPiece.moving = true;
+  }
+}
+
+function mouseReleased() {
+  if (movingPiece && movingPiece.white === whitesMove) {
+    const x = floor(mouseX / Board.TILE_SIZE);
+    const y = floor(mouseY / Board.TILE_SIZE);
+
+    if (movingPiece.canMove(x, y, false)) {
+      movingPiece.move(x, y);
+
+      // Here we switch the turn to the next player
+      whitesMove = !whitesMove;
+
+      // Here we execute some necessary board functionality, and basically update 
+      // the state of the board after the player has made a turn
+      board.run();
+    }
+
+    movingPiece.moving = false;
+  }
+}
